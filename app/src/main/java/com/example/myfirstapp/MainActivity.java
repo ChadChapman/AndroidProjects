f741c92ed9808c4234510aa77fdf5e20f4213c30
ChadTC <codecrab.me@gmail.com>
2018-04-03 11:19:37 -0700

    added sendMessage and new Activity for that intent

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.myfirstapp;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     TextView mTextView;
 
     String mAppState;
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +32,19 @@ public class MainActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_main);
 //        mTextView = (TextView) findViewById(R.id.text_view);
+
+        Log.i("onCreate", "MyFirstApp was created!");
+
     }
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
 //        mTextView.setText(savedInstanceState.getString(TEXT_VIEW_KEY));
+        if (savedInstanceState == null) {
+            Log.e("onRestoreInstanceState", "savedInstanceState was equal to null!");
+        }
+        Log.i("onRestoreInstanceState", "instance state was restored");
+        Log.d("onRestoreInstanceState", "Bundle= " + savedInstanceState.toString());
     }
 
     @Override
@@ -31,31 +53,49 @@ public class MainActivity extends AppCompatActivity {
 //        outState.putString(TEXT_VIEw_KEY, mTextView.getText());
 
         super.onSaveInstanceState(outState);
+        Log.w("onSaveInstanceState", "instance state is being saved!");
+        Log.d("onSaveInstanceState", "outState= " + outState.toString());
     }
 
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.v("onStart", "application is starting");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.v("onResume", "application is has now resumed");
+        Log.w("onResume", "resumed application");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.w("onPause", "application has been paused");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        ContentValues values = new ContentValues();
+//        values.put(NotePad.Notes.COLUMN_NAME_NOTE, getCurrentNoteText());
+//        values.put(NotePad.Notes.COLUMN_NAME_TITLE, getCurrentNoteTitle());
+        Log.w("onStop", "application has been stopped!");
+        if (values == null) {
+            Log.e("onStop", "uh-oh, content values were null!");
+        } else {
+            Log.e("onStop", "content values were not null!");
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.w("onDestory", "oh noes! app has been destroyed!");
+        Log.e("onDestroy", "app has been destroyed");
+        Log.wtf("onDestroy", "noooo!! you must never destroy this app!");
     }
 }
